87a8a46c50c74ce8e04f4f159d6362dd3a85eb27
A B O U D <aboudkalaf10@gmail.com>
2025-07-01 17:39:22 +0300

    fix background notifications exceptions -- v1

--- lib/User/Features/notifications/data/handlers/firebase_messaging_handlers.dart
+++ lib/User/Features/notifications/data/handlers/firebase_messaging_handlers.dart
@@ -5,6 +5,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:moatmat_app/User/Core/injection/app_inj.dart';
+import 'package:moatmat_app/User/Core/resources/supabase_r.dart';
 import 'package:moatmat_app/User/Features/notifications/domain/requests/register_device_token_request.dart';
 import 'package:moatmat_app/User/Features/notifications/domain/usecases/display_firebase_notification_usecase.dart';
 import 'package:moatmat_app/User/Features/notifications/domain/usecases/register_device_token_usecase.dart';
@@ -13,24 +14,28 @@ import 'package:moatmat_app/firebase_options.dart';
 
 ///
 /// [firebase messaging background handler]
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 @pragma('vm:entry-point')
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  debugPrint(
-      'A background message was received in flutter_background_service plugin: ${message.messageId}');
-
-  ///
+  debugPrint('A background message was received in flutter_background_service plugin: ${message.messageId}');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   debugPrint('Firebase initialized');
 
-  ///
-  await initGetIt();
+  await Supabase.initialize(
+    url: SupabaseResources.url,
+    anonKey: SupabaseResources.key,
+  );
+  
+  debugPrint('Supabase initialized');
 
-  ///
+  if (!locator.isRegistered<DisplayFirebaseNotificationUsecase>()) {
+    await initGetIt();
+  }
+
   await locator<DisplayFirebaseNotificationUsecase>().call(message: message);
-
   debugPrint('DisplayFirebaseNotificationUsecase called');
 }
 
